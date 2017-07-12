398296d2355164aef644d9f37a7daae157588cf5
Christopher Wong <christopherpk1996@gmail.com>
2017-07-12 12:21:57 +0800

    libs

--- kinect-cpp/main.cpp
+++ kinect-cpp/main.cpp
@@ -1,24 +1,34 @@
 #include "pk_kinect.h"
 
-#include <opencv2/core.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/features2d/features2d.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/nonfree/nonfree.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <gl/GL.h>
 
 GLubyte data[COLOR_WIDTH * COLOR_HEIGHT * 4];
 
 int main(int argc, char* argv[])
 {
-	Kinect* kinect = new Kinect();
-	kinect->initialize();
+	Kinect kinect;
 	cv::namedWindow("Kinect");
+	kinect.initialize();
 	while (1)
 	{
-		bool status = kinect->fetch();
-		if (status)
+		if (kinect.fetch())
 		{
-			kinect->getRgba(data);
-			cv::Mat img = cv::Mat(COLOR_HEIGHT, COLOR_WIDTH, CV_8UC4, data);
-			cv::imshow("Kinect", img);
+			cv::SiftFeatureDetector detector(500);
+			std::vector<cv::KeyPoint> keypoints;
+			keypoints.reserve(100000);
+			kinect.getRgba(data);
+			cv::Mat color(COLOR_HEIGHT, COLOR_WIDTH, CV_8UC4, data);
+			cv::Mat gray;
+			cv::cvtColor(color, gray, CV_BGRA2GRAY);
+			detector.detect(gray, keypoints);
+			cv::drawKeypoints(gray, keypoints, gray);
+			cv::imshow("Kinect", gray);
 		}
 		cv::waitKey(30);
 	}
